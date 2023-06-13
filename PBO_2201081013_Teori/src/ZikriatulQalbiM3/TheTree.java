8b1c089d3b7803eded7f8d355563cee3ef16eb5f
ZikriatulQalbi <126840470+ZikriatulQalbi@users.noreply.github.com>
2023-06-13 20:02:29 +0700

    Modul3

---
+++ PBO_2201081013_Teori/src/ZikriatulQalbiM3/TheTree.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ZikriatulQalbiM3;
+
+/**
+ *
+ * @author hp
+ */
+public class TheTree {
+    public static void main(String[]args){
+            System.out.println("I think that shall never see,");
+            System.out.println("a poem as lovely as a tree.");
+            System.out.println("A tree whose hungry mouth is pressed");
+            System.out.println("Againts the Earth's sweet flowing breast.");
+        }
+
+    
+}
