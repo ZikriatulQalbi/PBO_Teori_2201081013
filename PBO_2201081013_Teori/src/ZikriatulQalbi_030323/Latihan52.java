f6b7a4f90cca6378d316e01988f6e9fa5223df07
ZikriatulQalbi <126840470+ZikriatulQalbi@users.noreply.github.com>
2023-06-13 20:03:06 +0700

    Modul5

---
+++ PBO_2201081013_Teori/src/ZikriatulQalbi_030323/Latihan52.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ZikriatulQalbi_030323;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hp
+ */
+public class Latihan52 {
+    public static void main(String[] args){
+        
+        String Word1 = "";
+        Word1 =             
+        JOptionPane.showInputDialog("Enter Word 1 :");
+        
+        String Word2 = "";
+        Word2 = 
+        JOptionPane.showInputDialog("Enter Word 2 :");
+
+        
+        String msg = Word1 + " " + Word2 + " " + "Hello " + "!";
+        JOptionPane.showMessageDialog(null, msg);
+        
+    }
+    
+}
+
+    
+
